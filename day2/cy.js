8d8956a9d331d204859fbf7741a22d8fa132a7d5
ashwithasujitha <150882281+ashwithasujitha@users.noreply.github.com>
2024-08-07 08:52:55 +0530

    Create cy.js

---
+++ day2/cy.js
@@ -0,0 +1,15 @@
+import React from "react";
+const App8 = () =>{
+    return(
+        <div style={{backgroundColor:"lightblue"}}>
+            <h1 style={{textAlign:"center"}}>Car and Location Info</h1>
+            
+            
+            <h3 style={{textAlign:"center"}}>Tesla Model S</h3>
+            <img src="/car_image.png" alt="Tesla Model S" style={{alignItems:"center"}}></img>
+            <h3 style={{textAlign:"center"}}>Tesla</h3>
+            <p style={{textAlign:"center"}}>Palo Alto</p>
+        </div>
+    );
+};
+export default App8;
